b2a25be882fb8dda0922f9e3a9ad5a9eac8518d4
bartlomiej.czyz <bartlomiej.czyz@fabrykagier.pl>
2017-09-21 15:54:56 +0200

    Getting more data

--- routes/lapstat.js
+++ routes/lapstat.js
@@ -3,10 +3,6 @@ var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
 
 router.get('/', function (req, res) {
     var url = 'http://managerdc7.rackservice.org:50915/lapstat?valid=1';
@@ -15,19 +11,18 @@ router.get('/', function (req, res) {
     request(url, function (error, response, html) {
         if (!error) {
             var $ = cheerio.load(html);
-            // We'll use the unique header class as a starting point.
+
             $('table tbody').filter(function () {
 
-                // Let's store the data we filter into a variable so we can easily see what's going on.
-
                 var rows = $(this).children();
-                var driver,time;
 
                 rows.each(function (index, row) {
-                    driver = row.children[3].children[0].data;
-                    time = row.children[7].children[0].data;
-
-                    result.push({driver: driver, time: time});
+                    result.push({
+                        driver: row.children[3].children[0].data.trim(),
+                        car: row.children[5].children[2].data.trim(),
+                        time: row.children[7].children[0].data.trim(),
+                        gap:row.children[9].children[0].data.trim(),
+                    });
                 });
             })
         } else {
